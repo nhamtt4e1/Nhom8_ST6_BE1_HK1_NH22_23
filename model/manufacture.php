8202810d0d2160071a0ecb0520c539cb335e399f
nham <trannham05042002@gmail.com>
2022-11-25 23:23:59 +0700

    “nham7”

--- model/manufacture.php
+++ model/manufacture.php
@@ -10,4 +10,14 @@ class Manufacture extends Db
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items; //return an array
     }
+    public function getProductsByManuName($name)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM Products,Manufactures WHERE Products.'manu_id' = Manufactures.'manu_id' AND manu_name LIKE ?");
+        $keyword = "$name";
+        $sql->bind_param('s', $keyword);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
 }
---
+++ model/user.php
@@ -0,0 +1,62 @@
+<?php
+class User extends Db
+{
+    public function checkLogin($username, $password)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM `users` WHERE `username` = ? AND `password` = ?");
+        $password = md5($password);
+        $sql->bind_param("ss", $username, $password);
+        $sql->execute();
+        $items = array();
+        $items = $sql->get_result()->num_rows;
+        if ($items == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getRoleId($username)
+    {
+        $sql = self::$connection->prepare("SELECT `role_id` FROM `users` WHERE `username` =?");
+        $sql->bind_param("s", $username);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function getAllUsername()
+    {
+        $sql = self::$connection->prepare("SELECT `username` FROM `users`");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+  
+    public function getInfoByUsername($username)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM `users`,`roles` WHERE `username`=? AND `users`.`role_id`=`roles`.`role_id`");
+        $sql->bind_param("s", $username);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function updateUser($first_name, $last_name, $phone, $user_id)
+    {
+        $sql = self::$connection->prepare("UPDATE `users` SET `First_name`=?,`Last_name`=?,`phone`=? WHERE `user_id`=? ");
+        $sql->bind_param("ssii", $first_name, $last_name, $phone, $user_id);
+        return    $sql->execute(); //return an object
+
+    }
+  
+    public function getUserById($user_id)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM users WHERE user_id = " . $user_id);
+        $sql->execute(); //return an object
+        $item = array();
+        $item = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $item; //return an array
+    }
+}
